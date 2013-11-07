3b98d9f64bb4883a0ad97b6d8a186aca1d51a6bd
Miller Peterson <miller.peterson@gmail.com>
2013-11-07 14:21:04 -0500

    Fix logic of retrieving prev/next entries to take account of user
    Signed-off-by: Miller Peterson <miller.peterson@gmail.com>

--- app/models/gratefulness_entry.rb
+++ app/models/gratefulness_entry.rb
@@ -5,24 +5,26 @@ class GratefulnessEntry < ActiveRecord::Base
 
   belongs_to :author, class_name: 'User'
 
-  def self.previous_entry(date_ref)
-    where('creation_date < ?', date_ref)
+  def self.previous_entry(author, date_ref)
+    return nil if author.nil? || date_ref.nil?
+    where('author_id = ? AND creation_date < ?', author.id, date_ref)
     .order('creation_date DESC')
     .limit(1).first
   end
 
-  def self.next_entry(date_ref)
-    where('creation_date > ?', date_ref)
+  def self.next_entry(author, date_ref)
+    return nil if author.nil? || date_ref.nil?
+    where('author_id = ? AND creation_date > ?', author.id, date_ref)
     .order('creation_date ASC')
     .limit(1).first
   end
 
   def previous
-    GratefulnessEntry.previous_entry(self.creation_date)
+    GratefulnessEntry.previous_entry(self.author, self.creation_date)
   end
 
   def next
-    GratefulnessEntry.next_entry(self.creation_date)
+    GratefulnessEntry.next_entry(self.author, self.creation_date)
   end
 
 end
