cf84b6ffcd75da2df6fde350f8dc1b8075174cca
Miller Peterson <miller.peterson@gmail.com>
2013-11-09 13:00:33 -0500

    Add unit tests for user model
    Signed-off-by: Miller Peterson <miller.peterson@gmail.com>

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,8 +2,19 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Corey has 5 entries' do
+    corey = users(:corey)
+    assert_equal 5, corey.entries.size
+  end
+
+  test 'Trevor has 5 entries' do
+    trevor = users(:trevor)
+    assert_equal 5, trevor.entries.size
+  end
+
+  test 'Jim has no entries' do
+    jim = users(:jim)
+    assert_equal 0, jim.entries.size
+  end
 
 end
